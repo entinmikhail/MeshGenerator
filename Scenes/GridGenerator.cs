9f584a2a35a1f587df45522b047c206333569414
WORK-STATION-2\WORKSTATION-2 <entin@bastard.games>
2022-09-01 16:45:35 +0300

    generation add

--- Scenes/GridGenerator.cs
+++ Scenes/GridGenerator.cs
@@ -9,6 +9,9 @@ public class GridGenerator : MonoBehaviour
     [SerializeField] private Texture[] _1;
     [SerializeField] private Texture[] _1_n;
     [SerializeField] private GameObject _prefab;
+    [SerializeField] private GameObject _quad;
+    [SerializeField] private float _size = 5f;
+    [SerializeField] private int _indexToColor;
     
     private List<Vector3> _vertices = new ();
     private List<int> _triangles = new ();
@@ -22,23 +25,183 @@ public class GridGenerator : MonoBehaviour
     private static readonly int MainTex = Shader.PropertyToID("_MainTex");
     private static readonly int Layer1Tex = Shader.PropertyToID("_Layer1Tex");
     private List<GameObject> _pool = new ();
-    
-    private void Start()
+    private Dictionary<int, Triangle> _trianglesByIndex = new();
+    private Dictionary<int, List<int>> _meshIndexesByGlobalVertexIndex = new();
+    private Dictionary<int, Mesh> _meshByMeshIndex = new();
+    private Dictionary<int, Dictionary<int, int>> _meshIndexAndLocalVertexIndexPairByGlobalVertexIndex = new();
+
+    private void Awake()
     {
         _halfRadius = _radius / 2f;
         _meshHeight = _meshLength * 2;
         _height = Mathf.Sqrt(Mathf.Pow(_radius, 2) - Mathf.Pow(_halfRadius, 2));
-        _offset = new Vector3(0, _halfRadius);
-        Generate();
+        
+        for (var i = 0; i < 6; i++)
+        {
+            for (var j = 0; j < 8; j++)
+            {
+                var quadCenter = new Vector3(_size / 2 + j * _size, _size / 2 + i * _size);
+                var halfSize = _size / 2;
+
+                var a = new Vector3(quadCenter.x - halfSize, quadCenter.y + halfSize);
+                var b = new Vector3(quadCenter.x + halfSize, quadCenter.y + halfSize);
+                var c = new Vector3(quadCenter.x + halfSize, quadCenter.y - halfSize);
+                var d = new Vector3(quadCenter.x - halfSize, quadCenter.y - halfSize);
+                
+                _vertices.Add(a);
+                _vertices.Add(b);
+                _vertices.Add(c);
+                _vertices.Add(d);
+            }
+        }
     }
 
-    private void Generate()
-    {   
-        _mesh = new Mesh();
-        _prefab.GetComponent<MeshFilter>().mesh = _mesh;
-        _mesh.name = "Grid";
-        _vertices = new List<Vector3>(_meshLength * _meshHeight * 3);
-        _uv = new List<Vector2>(_meshLength * _meshHeight * 3);
+    private void Start()
+    {
+        GlobalGenerate();
+        
+        for (var i = 1; i > 0; i--)
+        {
+            for (var j = 0; j < 1; j++)
+            {
+                var spriteIndex = j + (1 * (1 - i));
+
+                var quadCenter = new Vector3(_size / 2 + j * _size, _size / 2 + i * _size);
+                var halfSize = _size / 2;
+                var go = Generate(quadCenter, _size, spriteIndex);
+                var render = go.GetComponent<MeshRenderer>();
+                
+                SetRendererTexture(render, MainTex, _1[spriteIndex]);
+                SetRendererTexture(render, Layer1Tex, _1_n[spriteIndex]);
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (_meshIndexAndLocalVertexIndexPairByGlobalVertexIndex.TryGetValue(_indexToColor, out var dict))
+        {
+            foreach (var meshIndexPair in dict)
+            {
+                if (_meshByMeshIndex.TryGetValue(meshIndexPair.Key, out var mesh))
+                {
+                    mesh.colors[meshIndexPair.Value] = Color.red;
+                    Debug.Log(mesh.vertices[meshIndexPair.Value]);
+                    Gizmos.DrawSphere(mesh.vertices[meshIndexPair.Value], 0.2f);
+                }
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (_vertices == null)
+        {
+            return;
+        }
+        Gizmos.color = Color.red;
+        for (int i = 0; i < _vertices.Count; i++)
+        {
+            Gizmos.DrawSphere(_vertices[i], 0.2f);
+        }
+    }
+    
+    private GameObject Generate(Vector3 position, float size, int meshIndex)
+    {
+        var mesh = new Mesh();
+        var go = Instantiate(_prefab);
+        go.GetComponent<MeshFilter>().mesh = mesh;
+        mesh.name = "Grid";
+        
+        _meshByMeshIndex.TryAdd(meshIndex, mesh);
+
+        var vertices = new List<Vector3>(_meshLength * _meshHeight * 3);
+        var triangles = new List<int>(_meshLength * _meshHeight * 3);
+        var uv = new List<Vector2>(_meshLength * _meshHeight * 3);
+        var colors = new List<Color>(_meshLength * _meshHeight * 3);
+
+        var index = 0;
+        foreach (var triangleByIndex in _trianglesByIndex)
+        {
+            foreach (var vertex in triangleByIndex.Value.Vertexes)
+            {
+                if (IsPointInQuad(vertex, position, size))
+                {
+                    List<Vector3> ver = new List<Vector3>();
+                    var leftBottomTileVertexWorldPosition = position - new Vector3(size / 2, size / 2);
+                    var rightRopTileVertexWorldPosition = position + new Vector3(size / 2, size / 2);
+                    var k = 0;
+                    foreach (var vert in triangleByIndex.Value.Vertexes)
+                    {
+
+                        if (_meshIndexAndLocalVertexIndexPairByGlobalVertexIndex.ContainsKey(triangleByIndex.Value.TriangleIndex))
+                        {
+                            _meshIndexAndLocalVertexIndexPairByGlobalVertexIndex[triangleByIndex.Value.TriangleIndex].TryAdd(meshIndex, vertices.Count + k);
+                        }
+                        else
+                        {
+                            var dict = new Dictionary<int, int> { { meshIndex, vertices.Count + k } };
+                            _meshIndexAndLocalVertexIndexPairByGlobalVertexIndex.TryAdd(triangleByIndex.Value.TriangleIndex, dict );
+                        }
+                        
+                        ver.Add(vert);
+                        colors.Add(Color.black);
+                        k++;
+                    }
+
+                  
+                    
+                    foreach (var v in ver)
+                    {
+                        var uvValue = new Vector2(GetUVValue(leftBottomTileVertexWorldPosition.x, rightRopTileVertexWorldPosition.x, v.x),
+                            GetUVValue(leftBottomTileVertexWorldPosition.y, rightRopTileVertexWorldPosition.y, v.y));
+
+                        uv.Add(uvValue);
+                    }
+
+                    for (int i = 0; i < ver.Count; i++)
+                    {
+                        ver[i] -= leftBottomTileVertexWorldPosition;
+                    }
+                    
+                    vertices.AddRange(ver);
+                    triangles.AddRange(GetTrianglesByVertex(ver.ToArray(), index, triangleByIndex.Value.IsRotated));
+                    index++;
+                    break;
+                }
+            }
+        }
+        
+        mesh.vertices = vertices.ToArray();
+        mesh.triangles = triangles.ToArray();
+        mesh.uv = uv.ToArray();
+        mesh.colors = colors.ToArray();
+        mesh.RecalculateNormals();
+        go.transform.position = position - new Vector3(size / 2, size / 2);
+        go.SetActive(true);
+
+        return go;
+    }
+
+    private float GetUVValue(float a, float b, float v)
+    {
+        var full = b - a;
+        var fullValue = v - a;
+        var one = full / 100;
+
+        return fullValue / one / 100f;
+    }
+
+    private GameObject GlobalGenerate()
+    {
+        var mesh = new Mesh();
+        _mesh = mesh;
+        var go = Instantiate(_prefab);
+        go.GetComponent<MeshFilter>().mesh = mesh;
+        mesh.name = "Grid";
+        var vertices = new List<Vector3>(_meshLength * _meshHeight * 3);
+        var triangles = new List<int>(_meshLength * _meshHeight * 3);
+        var uv = new List<Vector2>(_meshLength * _meshHeight * 3);
 
         for (int i = 0, index = 0; i < _meshHeight + 2; i++)
         {
@@ -46,54 +209,44 @@ public class GridGenerator : MonoBehaviour
             if (_subRow >= 5)
                 _subRow = 1;
             
-            var a = IsNeedToAddTriangle() ? _meshLength + 1 : _meshLength ;
+            RefreshForTriangle(i, _height, _halfRadius, i + 1);
             
-            GetOffsetForTriangle(i, _height, _halfRadius, i + 1);
-            for (int j = 0; j < a; j++, index++)
+            for (int j = 0; j < _meshLength; j++, index++)
             {
                 var newVertexes = OffsetTriangleByVector3(GetTriangleByRadius(_radius, IsNeedToRotate()), _offset);
-                _vertices.AddRange(newVertexes);
-
-                foreach (var vertex in newVertexes)
-                {
-                    if (vertex.x < 0 || vertex.y < 0)
-                    {
-                        Debug.LogError(vertex);
-                    }
-                }
-                _triangles.AddRange(GetTrianglesByVertex(newVertexes, index, IsNeedToRotate()));
-
-                foreach (var vertex in newVertexes)
-                {
-                   var uv = new Vector2(vertex.x / (_meshLength * _height * 2), vertex.y / ((_meshHeight) * _height));
-                   
-                    _uv.Add(uv);
-                }
-
+                vertices.AddRange(newVertexes);
+                var newTriangle = GetTrianglesByVertex(newVertexes, index, IsNeedToRotate());
+                triangles.AddRange(newTriangle);
+                _trianglesByIndex.Add(index, new Triangle(index, newVertexes, newTriangle, IsNeedToRotate()));
+                
                 _offset += new Vector3(_height * 2, 0);
             }
         }
 
-        _mesh.vertices = _vertices.ToArray();
-        _mesh.triangles = _triangles.ToArray();
-        _mesh.uv = _uv.ToArray();
-        _mesh.RecalculateNormals();
-        
-        _prefab.SetActive(false);
+        mesh.vertices = vertices.ToArray();
+        mesh.triangles = triangles.ToArray();
+        mesh.RecalculateNormals();
+        go.SetActive(false);
+        return go;
+    }
 
-        for (var i = 0; i < 2; i++)
-        {
-            for (int j = 0; j < 2; j++)
-            {
-                var go = Instantiate(_prefab, new Vector3((_meshLength) * _height * 2 * j, i * _radius * 2 * (_meshLength - 1)), Quaternion.identity);
-            
-                SetRendererTexture(go.GetComponent<MeshRenderer>(), MainTex, _1[i + j]);
-                SetRendererTexture(go.GetComponent<MeshRenderer>(), Layer1Tex, _1_n[i + j]);
-                go.SetActive(true);
-                _pool.Add(go);
-            }
-            
-        }
+    private bool IsPointInQuad(Vector3 point, Vector3 quadCenter, float quadSize)
+    {
+        var halfSize = quadSize / 2;
+        var a = new Vector3(quadCenter.x - halfSize, quadCenter.y + halfSize);
+        var b = new Vector3(quadCenter.x + halfSize, quadCenter.y + halfSize);
+        var c = new Vector3(quadCenter.x + halfSize, quadCenter.y - halfSize);
+        var d = new Vector3(quadCenter.x - halfSize, quadCenter.y - halfSize);
+
+
+        var inArea = Side(a, b, point) <= 0 
+                     && Side(b, c, point) <= 0 
+                     && Side(c, d, point) <= 0 
+                     && Side(d, a, point) <= 0;
+
+        return inArea;
+        
+        float Side(Vector3 a, Vector3 b, Vector3 p) => Mathf.Sign((b.x - a.x) * (p.y - a.y) - (b.y - a.y) * (p.x - a.x));
     }
     
     private bool IsNeedToAddTriangle()
@@ -131,7 +284,7 @@ public class GridGenerator : MonoBehaviour
         return result;
     }
 
-    private void GetOffsetForTriangle(int i, float height, float halfRadius, int row)
+    private void RefreshForTriangle(int i, float height, float halfRadius, int row)
     {
         switch (_subRow)
         {
@@ -209,5 +362,21 @@ public class GridGenerator : MonoBehaviour
         }
         
         return hex;
+    }
+    
+    private class Triangle
+    {
+        public int TriangleIndex; 
+        public Vector3[] Vertexes;
+        public int[] VertexesIndex;
+        public bool IsRotated;
+        
+        public Triangle(int triangleIndex, Vector3[] vertexes, int[] vertexesIndex, bool isRotated)
+        {
+            TriangleIndex = triangleIndex;
+            Vertexes = vertexes;
+            VertexesIndex = vertexesIndex;
+            IsRotated = isRotated;
+        }
     }
 }
